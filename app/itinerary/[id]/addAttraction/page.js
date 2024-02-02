73e436d5f2c1fe58335cc664cf9e397fc81406e7
AdamC7313 <AdamC7313@gmail.com>
2024-02-01 19:54:31 -0500

    add search bar

--- app/itinerary/[id]/addAttraction/page.js
+++ app/itinerary/[id]/addAttraction/page.js
@@ -6,9 +6,13 @@ import { useSelector } from 'react-redux';
 const AddAttraction = () => {
   const dispatch = useDispatch();
   const trips = useSelector((state) => state.trips.trips);
-  console.log(trips[0])
   return (
-    <div>{trips[0].id}</div>
+    <div className='w-100 d-flex justify-content-center align-items-center'>
+      <div className="searchbar d-flex w-100 justify-content-center align-items-center">
+        <input className='form-control search mx-3 w-50'></input>
+        <button className='btn btn-primary searchButton'>Search</button>
+      </div>
+    </div>
   )
 }
 
