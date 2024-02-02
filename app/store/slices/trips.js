30136f1d0840bc157210832a7c56cb93d5d84bc2
Ryan Hardin <rhardin378@gmail.com>
2024-02-02 17:41:05 -0600

    added lat and long to intial state

--- app/store/slices/trips.js
+++ app/store/slices/trips.js
@@ -15,6 +15,8 @@ export const tripsSlice = createSlice({
         hotel: {
           name: "Marriott",
           address: "1000 Boylston St, Boston, MA 02116",
+          lat: 42.346741,
+          lon: -71.078049,
           rating: 4,
           link: "https://www.marriott.com/hotels/travel/bosco-boston-marriott-copley-place/",
         },
