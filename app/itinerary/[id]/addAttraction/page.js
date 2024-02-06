d09e5f28350b4b3802a1aafc119ee403f543ee3e
AdamC7313 <AdamC7313@gmail.com>
2024-02-05 19:17:34 -0500

    add table for attractions

--- app/itinerary/[id]/addAttraction/page.js
+++ app/itinerary/[id]/addAttraction/page.js
@@ -15,14 +15,30 @@ const AddAttraction = () => {
   const trip = trips.find((trip) => trip.id === parseInt(id));
 
   return (
-    <div className='w-100 text-center'>
-    <h1 className='text-'>Add Attraction for {trip.city}</h1>
+    <div className='w-100 text-center d-flex flex-column justify-content-center align-items-center'>
+    <h1>Add Attraction for {trip.city}</h1>
     <div className='w-100 d-flex justify-content-center align-items-center'>
       <div className="searchbar d-flex w-100 justify-content-center align-items-center">
         <input className='form-control search mx-3 w-50'></input>
         <button className='btn btn-primary searchButton'>Search</button>
       </div>
     </div>
+    <table className='table w-75 mt-5'>
+        <thead>
+          <tr>
+            <th>Attraction</th>
+            <th>Distance from Hotel</th>
+            <th>Address</th>
+            <th>Rating</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>Hi</td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
